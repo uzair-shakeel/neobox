4cb56174bc0a968bf397667d004d26d61dc257ae
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-09-26 21:36:30 +0500

    structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUp from "./pages/Signup"; // Ensure the path is correct
+import SignUp from "./pages/authentication/Signup"; // Ensure the path is correct
 import "./App.css";
-import CreatePassword from "./pages/CreatePassword";
-import OTP from "./pages/OTP";
-import Verify from "./pages/Verify";
-import Success from "./pages/Success";
+import CreatePassword from "./pages/authentication/CreatePassword";
+import OTP from "./pages/authentication/OTP";
+import Verify from "./pages/authentication/Verify";
+import Success from "./pages/authentication/Success";
+import Preferences from "./pages/set-up-store/Preference";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/otp" element={<OTP />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/success" element={<Success />} />
+          <Route path="/store/preference" element={<Preferences />} />
           {/* You can add more routes here */}
         </Routes>
       </div>

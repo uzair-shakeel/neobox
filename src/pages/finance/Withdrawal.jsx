4a10dd39316c1a09d639daf14d0a4a2af913e278
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-10-04 04:49:41 +0500

    withdrawal Page responsive

--- src/pages/finance/Withdrawal.jsx
+++ src/pages/finance/Withdrawal.jsx
@@ -23,32 +23,36 @@ const Withdrawal = () => {
 
         {/* Main Content */}
         <div className="flex-1 px-8 py-6">
-          <h2 className="text-3xl font-semibold mb-6">Withdrawal Methods</h2>
+          <h2 className="text-2xl lg:text-3xl font-semibold mb-6">
+            Withdrawal Methods
+          </h2>
 
           <div className="grid grid-cols-12 gap-6">
             {/* Left Content - Saved Withdrawal Methods */}
-            <div className="col-span-6">
+            <div className="col-span-12 lg:col-span-6">
               <h3 className="text-lg font-semibold mb-4">
                 Saved withdrawal methods
               </h3>
-              <div className="bg-white border border-gray-200 p-6 rounded-lg shadow-sm mb-6">
-                <div className="flex justify-between items-center border border-gray-200 rounded-lg p-4">
-                  <div className="flex items-center space-x-4">
+              <div className="bg-white border border-gray-200 p-3 lg:p-6 rounded-lg shadow-sm mb-6">
+                <div className="flex justify-between items-center border border-gray-200 rounded-lg p-2 lg:p-4">
+                  <div className="flex items-center space-x-2 lg:space-x-4">
                     <img
                       src="/bank.svg"
                       alt="Bank Icon"
                       className="border border-gray-100 p-2 rounded-full"
                     />
                     <div>
-                      <p className="font-semibold">Bank Transfer • CAD</p>
-                      <p className="text-sm text-gray-500">
+                      <p className="font-semibold text-[13px] lg:text-[16px]">
+                        Bank Transfer • CAD
+                      </p>
+                      <p className="text-[10px] lg:text-[14px] text-gray-500">
                         marquezemarc@gmail.com
                       </p>
                     </div>
                   </div>
                   <img src="/arrows.svg" alt="Arrow Icon" />
                 </div>
-                <p className="text-sm text-gray-600 mt-4">
+                <p className="text-[13px] lg:text-sm text-gray-600 mt-4">
                   Use them to withdraw your funds from Neobox once a payment is
                   received. Click on each method to customize or view details.
                 </p>
@@ -56,7 +60,7 @@ const Withdrawal = () => {
                 {/* Add Method Button */}
                 <button
                   onClick={openModal}
-                  className="flex mt-4 items-center space-x-2 font-semibold py-2 px-5 border border-gray-300 rounded-lg hover:bg-gray-100 transition-colors"
+                  className="flex mt-4 w-full items-center justify-center lg:w-auto space-x-2 font-semibold py-2 px-5 border border-gray-300 text-[14px] lg:text-[17px] rounded-lg hover:bg-gray-100 transition-colors"
                 >
                   <HiOutlinePlusSm /> <span>Add Method</span>
                 </button>
@@ -64,13 +68,13 @@ const Withdrawal = () => {
             </div>
 
             {/* Right Content - Help Section */}
-            <div className="col-span-6">
+            <div className="col-span-12 lg:col-span-6">
               <h3 className="text-lg font-semibold mb-4">
                 How to withdraw your money?
               </h3>
-              <div className="bg-[#F5F3FF] rounded-lg p-6 grid grid-cols-2 justify-between">
+              <div className="bg-[#F5F3FF] rounded-lg p-6 grid lg:grid-cols-2 gap-8 lg:gap-2 justify-between">
                 <div>
-                  <h3 className="text-[24px] font-semibold mb-3">
+                  <h3 className="text-[18px] lg:text-[24px] font-semibold mb-3">
                     Get your money sent to multiple accounts automatically.
                   </h3>
                   <p className="text-[14px] text-gray-600 mb-4">
@@ -78,14 +82,14 @@ const Withdrawal = () => {
                   </p>
 
                   <div className="flex justify-between items-center mt-6">
-                    <button className="bg-white border border-gray-100 hover:bg-gray-100 transition-colors rounded-lg p-4 font-semibold flex items-center space-x-2 ">
+                    <button className="bg-white border w-full justify-center lg:w-auto border-gray-100 hover:bg-gray-100 transition-colors text-[14px] lg:text-[17px] rounded-lg p-4 font-semibold flex items-center space-x-2 ">
                       <span>Learn more</span>
                       <MdArrowOutward size={20} />
                     </button>
                   </div>
                 </div>
 
-                <div className="flex justify-end">
+                <div className="flex justify-center lg:justify-end">
                   <img
                     src="/coins.svg"
                     alt="Money Icon"

1e93a6d7002e32aa16713f262fce6ecb3c5f1e60
ShaYri12 <sharjeelb60@gmail.com>
2024-10-04 02:35:43 +0500

    fix modal

--- src/components/finance/AddPaymentModal.jsx
+++ src/components/finance/AddPaymentModal.jsx
@@ -58,7 +58,7 @@ const AddMethodModal = ({ isOpen, onClose }) => {
 
       {/* Modal Content */}
       <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="relative bg-white w-[400px] p-6 rounded-lg shadow-lg">
+        <div className="relative bg-white max-w-[420px] w-full p-6 rounded-lg shadow-lg max-h-[90vh] overflow-y-auto">
           {/* Close Button */}
           <button
             className="absolute top-6 right-4 text-gray-500 hover:text-gray-900"
